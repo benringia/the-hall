9bee3115d5f9ad9c90ed1ef50d647f0296c6453f
Ben Ringia <benringia@gmail.com>
2021-05-08 12:45:13 +0800

    Update main.js

--- wp-content/plugins/rest-countries-custom-plugin/js/main.js
+++ wp-content/plugins/rest-countries-custom-plugin/js/main.js
@@ -1,20 +1,6 @@
 
 window.onload=function(){
-    // let countries;
-
-// fetch("https://restcountries.eu/rest/v2/all")
-// .then(res => res.json())
-// .then(data => initialize(data))
-// .catch (err => console.log("Error: ",  err));
-
-// function initialize(countriesData) {
-//     countries = countriesData;
-//     let option ="";
-//     for (let i=0; i<countries.length; i++) {
-//         options += `<option value = "${countries[i].alpha3Code}">${countries[i].name}</option>`;
-//     }
-
-// }
+    
 
 
 // Global Variables
@@ -30,19 +16,6 @@ function newCountrySelection(event) {
   displayCountryInfo(event.target.value);
 }
 
-// fetch("https://restcountries.eu/rest/v2/all")
-// .then(function(res){
-//   // console.log(res);
-//   return res.json();
-// })
-// .then(function(data){
-//   // console.log(data);
-//   initialize(data);
-// })
-// .catch(function(err){
-//   console.log("Error:", err);
-// });
-
 fetch("https://restcountries.eu/rest/v2/all")
 .then(res => res.json())
 .then(data => initialize(data))
@@ -51,21 +24,10 @@ fetch("https://restcountries.eu/rest/v2/all")
 function initialize(countriesData) {
   countries = countriesData;
   let options = "";
-  // for(let i=0; i<countries.length; i++) {
-  //   options += `<option value="${countries[i].alpha3Code}">${countries[i].name}</option>`;
-  //   // options += `<option value="${countries[i].alpha3Code}">${countries[i].name} (+${countries[i].callingCodes[0]})</option>`;
-  // }
+ 
   countries.forEach(country => options+=`<option value="${country.alpha3Code}">${country.name}</option>`);
-  // document.getElementById("countries").innerHTML = options;
-  // document.querySelector("#countries").innerHTML = options;
+  
   countriesList.innerHTML = options;
-  // console.log(countriesList);
-  // console.log(countriesList.value);
-  // console.log(countriesList.length);
-  // console.log(countriesList.selectedIndex);
-  // console.log(countriesList[10]);
-  // console.log(countriesList[10].value);
-  // console.log(countriesList[10].text);
   countriesList.selectedIndex = Math.floor(Math.random()*countriesList.length);
   displayCountryInfo(countriesList[countriesList.selectedIndex].value);
 }
